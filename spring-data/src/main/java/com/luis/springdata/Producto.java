64d0dc3de9953f464d648b41a7b48f4724ff88b5
Luis Rodríguez <luisrodriguezcamas99@gmail.com>
2025-12-03 19:01:57 +0100

    fundamentals: @Table annotation & @GeneratedValue

--- spring-data/src/main/java/com/luis/springdata/Producto.java
+++ spring-data/src/main/java/com/luis/springdata/Producto.java
@@ -1,7 +1,6 @@
 package com.luis.springdata;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,9 +10,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+//@Table(name = "productos") Podemos modificar el nombre de la tabla, hay algunas palabras reservadas como
+// "Users" en PostgresSQL con las que si nuestra clase Java se llama igual, debemos de cambiar el nombre.
 public class Producto {
-    
+    // La estrategia AUTO elige la estrategia en función del gestor de DB y el tipo de dato que sea la PK.
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String nombre;
